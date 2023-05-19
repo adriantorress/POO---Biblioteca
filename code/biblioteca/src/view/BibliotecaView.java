3a4f860a6c6440e51e4629e77a23154f7aaf930e
Adrian <adriantorrescv@gmail.com>
2023-05-19 05:52:34 -0300

    Add Library View

---
+++ code/biblioteca/src/view/BibliotecaView.java
@@ -0,0 +1,24 @@
+package view;
+
+import java.util.Scanner;
+
+public class BibliotecaView {
+    private Scanner scanner;
+
+    public BibliotecaView() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void exibirMenu() {
+        System.out.println("----- Biblioteca -----");
+        System.out.println("1. Login ");
+        System.out.println("2. Cadastro ");
+        System.out.println("0. Sair");
+        System.out.print("Escolha uma opção: ");
+    }
+
+    public int lerOpcao() {
+        return scanner.nextInt();
+    }
+}
+
