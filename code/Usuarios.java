00a114247ccd72f9fd927acdb359b5ae928ffbdd
Adrian <adriantorrescv@gmail.com>
2023-05-19 04:37:40 -0300

    Add Project Proposal

---
+++ code/Usuarios.java
@@ -0,0 +1,21 @@
+'''
+Critérios de Avaliação dos Projetos
+
+Os seguintes critérios serão adotados na avaliação dos projetos:
+
+- utilizar classes abstratas, classes não abstratas, e interfaces;
+- uso adequado da OO: classes e encapsulamento;
+- uso de construtores;
+- aplicação do conceito de herança;
+- uso de Polimorfismo;
+- uso de Collections;
+- uso de Testes Unitários automatizados para cada regra de negócio (BO) e acesso ao BD (DAO);
+- uso de banco de dados (insert, update, delete, select);
+- separação do projeto em camadas (View, BO, VO, DAO);
+- uso de exceções (deve-se cria exceções próprias e tratá-las corretamente).
+- O uso de interface web
+
+
+Opcional:
+* Projeto estar no projeto no GitHub (não é para colocar o projeto final apenas, mas o projeto desde o início, para que possa ver os commits que cada integrante fez);
+* O uso de interface mobile ou web é OPCIONAL.'''
