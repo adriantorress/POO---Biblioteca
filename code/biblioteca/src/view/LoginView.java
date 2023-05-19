1f2ca610bfa715c686babae45b685a0aa18a7e3d
Adrian <adriantorrescv@gmail.com>
2023-05-19 15:32:38 -0300

    Add Login View

---
+++ code/biblioteca/src/view/LoginView.java
@@ -0,0 +1,19 @@
+package view;
+
+import bo.LoginBO;
+import java.util.Scanner;
+
+public class LoginView {
+    public static void exibirFormulario() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("---- Login ----");
+        System.out.print("Nome de usuário: ");
+        String username = scanner.nextLine();
+        System.out.print("Senha: ");
+        String password = scanner.nextLine();
+
+        scanner.close();
+
+        LoginBO.realizarLogin(username, password);
+    }
+}
