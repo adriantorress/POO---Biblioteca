e606cd2475342fd190585011c0f93adbf792b9dc
Adrian <adriantorrescv@gmail.com>
2023-05-21 00:16:50 -0300

    Update Librarian View to Admin View

---
+++ code/biblioteca/src/view/AdminView.java
@@ -0,0 +1,24 @@
+package view;
+import java.util.Scanner;
+
+public class AdminView {
+    private Scanner scanner;
+
+    public AdminView() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void exibirMenu() {
+        System.out.println("----- AdminView -----");
+        System.out.println("1. Cadastrar livro");
+        System.out.println("2. Atualizar livro");
+        System.out.println("3. Excluir livro");
+        System.out.println("4. Buscar livro");
+        System.out.println("0. Sair");
+        System.out.print("Escolha uma opção: ");
+    }
+
+    public int lerOpcao() {
+        return scanner.nextInt();
+    }
+}
