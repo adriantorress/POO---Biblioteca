8a79d92ee930d1b83c51b26cc8920311083efbe6
Adrian <adriantorrescv@gmail.com>
2023-05-21 15:11:58 -0300

    Update Sign Up BO

--- code/biblioteca/src/bo/CadastroBO.java
+++ code/biblioteca/src/bo/CadastroBO.java
@@ -3,36 +3,29 @@ package bo;
 import dao.CadastroDAO;
 
 public class CadastroBO {
-  public static void cadastrarUsuario(String username, String password) {
-      CadastroDAO.salvarUsuario(username, password);
-  }
+    public static void cadastrarUsuario(String name, String username, String password) {
+        boolean isCamposVazios = verificarCamposVazios(name,username, password);
+        boolean isUsuarioNaoCadastrado = verificarUsuarioNaoCadastrado(username);
+        if (isCamposVazios && isUsuarioNaoCadastrado) {
+            CadastroDAO.salvarUsuario(username, password);
+        }
+        ;
+    }
 
+    private static boolean verificarCamposVazios(String name,String username, String password) {
+        if (name.isEmpty() || username.isEmpty() || password.isEmpty()) {
+            System.out.println("Nome, usuário ou senha não preenchidos.");
+            return false;
+        }
+        return true;
+    }
 
-  private static boolean verificarCamposVazios(String username, String password) {
-      if (username.isEmpty() || password.isEmpty()) {
-          System.out.println("Nome de usuário e senha devem ser preenchidos.");
-          return false;
-      }
-      return true;
-  }
+    private static boolean verificarUsuarioNaoCadastrado(String username) {
+        if (CadastroDAO.verificarUsuarioCadastrado(username)) {
+            System.out.println("Usuário já cadastrado.");
+            return false;
+        }
+        return true;
+    }
 
-  private static boolean verificarUsuarioNaoCadastrado(String username) {
-      if (CadastroDAO.verificarUsuarioCadastrado(username)) {
-          System.out.println("Nome de usuário já cadastrado.");
-          return false;
-      }
-      return true;
-  }
-  
-  public static boolean isCamposVazios(String username, String password) {
-    return verificarCamposVazios(username, password);
-  }
-
-  public static boolean isUsuarioNaoCadastrado(String username) {
-      return verificarUsuarioNaoCadastrado(username);
-    
-    
-    
-  }
-  
 }
